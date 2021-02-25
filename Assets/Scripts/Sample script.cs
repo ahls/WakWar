47bf9b0faa528272ad10f2f4d71d17406ae9d79c
Ahlon Sin <ahls@uci.edu>
2021-02-24 21:57:38 -0800

    webhook test

--- Assets/Scripts/Sample script.cs
+++ Assets/Scripts/Sample script.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TwitchLib;
+using TwitchLib.Api;
 public class Samplescript : MonoBehaviour
 {
     // Start is called before the first frame update
