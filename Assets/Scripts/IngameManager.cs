73f3e861d656f1a3a95d64354766545b737d58fa
mjun1208 <mjun1208@gmail.com>
2021-03-02 00:42:29 +0900

    인게임 매니저 추가  - 이제 부턴 인게임에 관련된 매니저를 전부 인게임 매니저에서 관리하도록 하겠습니다

---
+++ Assets/Scripts/IngameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class IngameManager : MonoBehaviour
+{
+    public static IngameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                return null;
+            }
+
+            return _instance;
+        }
+    }
+    public static UnitSelector UnitSelector => _unitSelector;
+
+    private static IngameManager _instance;
+    private static UnitSelector _unitSelector;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void SetUnitSelector(UnitSelector unitSelector)
+    {
+        _unitSelector = unitSelector;
+    }
+}
