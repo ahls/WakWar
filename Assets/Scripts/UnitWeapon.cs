0cf2382d9da1bdfc2288b60867bb7751e91eef10
mjun1208 <mjun1208@gmail.com>
2021-03-06 02:30:30 +0900

    무기 기본 틀만 생성

---
+++ Assets/Scripts/UnitWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WeaponType
+{
+    Warrior,
+    Shooter,
+    Supporter
+}
+
+public class UnitWeapon : MonoBehaviour
+{
+}
