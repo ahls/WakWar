f4280d63ecb0b574859c493e560ac44ebf171c46
mjun1208 <mjun1208@gmail.com>
2021-03-07 03:36:21 +0900

    알람 메세지, 알람 창 스크립트 추가

---
+++ Assets/Scripts/UI/UINotifyText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UINotifyText : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+    private float _lifeTime;
+
+    public void SetInfo(string text, float lifeTime)
+    {
+        _text.text = text;
+        _lifeTime = lifeTime;
+
+        StartCoroutine(ShowMsg());
+    }
+
+    private IEnumerator ShowMsg()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+
+        Destroy(this.gameObject);
+    }
+}
