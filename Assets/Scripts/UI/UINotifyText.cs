72e0890812c816ee823024d40aa7557b82fce196
mjun1208 <mjun1208@gmail.com>
2021-03-07 20:54:45 +0900

    LifeTime을 메세지가 떠있는 시간으로 수정  - LifeTime이 끝나면 부식 시작

--- Assets/Scripts/UI/UINotifyText.cs
+++ Assets/Scripts/UI/UINotifyText.cs
@@ -7,31 +7,38 @@ public class UINotifyText : MonoBehaviour
 {
     [SerializeField] private Text _text;
     private float _lifeTime;
-    private float decayRate;
+
+    private const float decayRate = 2f;
+    private const float risingSpeed = 20f;
 
     public void SetInfo(string text, float lifeTime)
     {
         _text.text = text;
         _lifeTime = lifeTime;
-        decayRate = 1 / (50 * _lifeTime);
+
         StartCoroutine(ShowMsg());
     }
 
-    private void FixedUpdate()
+    private bool decay()
     {
-        decay();
-    }
+        this.transform.position += new Vector3(0, risingSpeed * (decayRate * Time.deltaTime), 0);
+        _text.color -= new Color(0, 0, 0, decayRate * Time.deltaTime);
 
-    private void decay()
-    {
-        //text.color 는 레퍼런스가 아니라 카피를 하는거같아요.. 그래서 미리 빼둘수가 없습니다.
-        _text.rectTransform.position += Vector3.up;
-        _text.color -= new Color(0, 0, 0, decayRate); 
+        if (_text.color.a < decayRate * Time.deltaTime)
+        {
+            return false;
+        }
+
+        return true;
     }
 
     private IEnumerator ShowMsg()
     {
         yield return new WaitForSeconds(_lifeTime);
+
+        while (decay()) {
+            yield return null;
+        }
 
         Destroy(this.gameObject);
     }
