35ffa381158955aba7ebe8ead051fa21adc7d255
mjun1208 <mjun1208@gmail.com>
2021-03-07 15:14:52 +0900

    줄정리

--- Assets/Scripts/UI/UINotifyText.cs
+++ Assets/Scripts/UI/UINotifyText.cs
@@ -8,6 +8,7 @@ public class UINotifyText : MonoBehaviour
     [SerializeField] private Text _text;
     private float _lifeTime;
     private float decayRate;
+
     public void SetInfo(string text, float lifeTime)
     {
         _text.text = text;
@@ -15,15 +16,19 @@ public class UINotifyText : MonoBehaviour
         decayRate = 1 / (50 * _lifeTime);
         StartCoroutine(ShowMsg());
     }
+
     private void FixedUpdate()
     {
         decay();
     }
+
     private void decay()
-    {//text.color 는 레퍼런스가 아니라 카피를 하는거같아요.. 그래서 미리 빼둘수가 없습니다.
+    {
+        //text.color 는 레퍼런스가 아니라 카피를 하는거같아요.. 그래서 미리 빼둘수가 없습니다.
         _text.rectTransform.position += Vector3.up;
         _text.color -= new Color(0, 0, 0, decayRate); 
     }
+
     private IEnumerator ShowMsg()
     {
         yield return new WaitForSeconds(_lifeTime);
