33d3f64452bb5ac1b0c734efa960557f1e593bf6
mjun1208 <mjun1208@gmail.com>
2021-03-10 01:22:12 +0900

    리소스 매니저, 오브젝트 풀링 매니저 추가  - 정말 스크립트 추가만했음요!!!! 구현은 나중에 ㅎㅎ~

---
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager
+{
+    private const string assetBundlePath = "";
+    private AssetBundle assetBundle;
+
+    private Dictionary<string, GameObject> _loadedObjectDic;
+
+    public GameObject LoadPrefab(string path)
+    {
+        assetBundle = AssetBundle.LoadFromFile(assetBundlePath);
+
+        if (_loadedObjectDic.ContainsKey(path))
+        {
+            return _loadedObjectDic[path];
+        }
+
+         var prefab = assetBundle.LoadAsset<GameObject>(path);
+
+        _loadedObjectDic[path] = prefab;
+
+        return prefab;
+    }
+}
