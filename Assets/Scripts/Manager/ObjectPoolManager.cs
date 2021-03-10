267e6a378dfa6a94d8bbbee52c0e1ee4b716e42a
mjun1208 <mjun1208@gmail.com>
2021-03-11 01:06:05 +0900

    오브젝트 풀링 구현

--- Assets/Scripts/Manager/ObjectPoolManager.cs
+++ Assets/Scripts/Manager/ObjectPoolManager.cs
@@ -1,5 +1,58 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class ObjectPoolManager : MonoBehaviour
 {
+    private Dictionary<string, GameObject> _objectPoolDic;
+    private Dictionary<string, GameObject> _globalObjectPoolDic;
+
+    public void ObjectPooling(string path, GameObject gameObject)
+    {
+        gameObject.SetActive(false);
+        gameObject.transform.parent = this.transform;
+        _objectPoolDic.Add(path, gameObject);
+    }
+
+    public void GlobalObjectPooling(string path, GameObject gameObject)
+    {
+        gameObject.SetActive(false);
+        gameObject.transform.parent = this.transform;
+        _globalObjectPoolDic.Add(path, gameObject);
+    }
+
+    public GameObject GetObject(string path)
+    {
+        if (_objectPoolDic.ContainsKey(path))
+        {
+            var pooledObject = _objectPoolDic[path];
+            _objectPoolDic.Remove(path);
+
+            return pooledObject;
+        }
+
+        return null;
+    }
+
+    public GameObject GetGlobalObject(string path)
+    {
+        if (_globalObjectPoolDic.ContainsKey(path))
+        {
+            var pooledObject = _globalObjectPoolDic[path];
+            _globalObjectPoolDic.Remove(path);
+
+            return pooledObject;
+        }
+
+        return null;
+    }
+
+    public void ResetObjectPool()
+    {
+        foreach(var destoryObject in _objectPoolDic)
+        {
+            Destroy(destoryObject.Value);
+        }
+
+        _objectPoolDic.Clear();
+    }
 }
