9812e67b22f459bd529304b88c77cbf50be0acb3
mjun1208 <mjun1208@gmail.com>
2021-03-13 03:45:52 +0900

    풀 오브젝트 데이터 클래스 추가

---
+++ Assets/Scripts/Manager/ObjectPoolData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolData
+{
+    private Queue<GameObject> gameObjectsQueue = new Queue<GameObject>();
+
+    public GameObject GetObject()
+    {
+        return gameObjectsQueue.Dequeue();
+    }
+    
+    public void PoolingObject(GameObject poolObject)
+    {
+        gameObjectsQueue.Enqueue(poolObject);
+    }
+
+    public int GetPoolObjectCount()
+    {
+        return gameObjectsQueue.Count;
+    }
+}
