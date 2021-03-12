abde1a12e01b6c93744aa61c669b9301b9e1a0fb
mjun1208 <mjun1208@gmail.com>
2021-03-13 03:38:21 +0900

    에셋번들을 stramingAsset폴더에서 로딩하도록 수정  - 빌드때 에셋 번들이 포함됨

--- Assets/Scripts/Manager/ResourceManager.cs
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -1,30 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ResourceManager
 {
-    private const string assetBundlePath = "Assets/AssetBundles/WakAsset";
+    private string assetBundlePath = "Assets/StreamingAssets/WakAsset";
     private AssetBundle assetBundle;
 
     private Dictionary<string, GameObject> _loadedObjectDic = new Dictionary<string, GameObject>();
+
+    public ResourceManager()
+    {
+#if UNITY_EDITOR
+        assetBundlePath = "Assets/StreamingAssets/WakAsset";
+#else
+        assetBundlePath = Application.dataPath + "/StreamingAssets/WakAsset";
+#endif
+        if (assetBundle == null)
+        {
+            AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(assetBundlePath));
+            assetBundle = request.assetBundle;
+            //AssetBundle.LoadFromFile(assetBundlePath);
+        }
+    }
 
     public GameObject LoadPrefab(string path)
     {
         if (assetBundle == null)
         {
-            assetBundle = AssetBundle.LoadFromFile(assetBundlePath);
+            throw new Exception("AseetBundle is Null");
         }
-
+       
         if (_loadedObjectDic.ContainsKey(path))
         {
             return _loadedObjectDic[path];
         }
 
-         var prefab = assetBundle.LoadAsset<GameObject>(path);
+        var prefab = assetBundle.LoadAsset<GameObject>(path);
 
         _loadedObjectDic[path] = prefab;
-
+        
         return prefab;
     }
 }
