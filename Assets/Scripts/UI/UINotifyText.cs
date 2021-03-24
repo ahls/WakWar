42deaf96bf1a9962902ec21bf9e855c1e48f7a16
mjun1208 <mjun1208@gmail.com>
2021-03-25 02:43:39 +0900

    NotifiyText가 풀에서 다시 불려와질때 리셋 되도록 수정

--- Assets/Scripts/UI/UINotifyText.cs
+++ Assets/Scripts/UI/UINotifyText.cs
@@ -10,9 +10,17 @@ public class UINotifyText : MonoBehaviour
 
     private const float decayRate = 2f;
     private const float risingSpeed = 20f;
+    private Vector2 _resetPos;
+
+    private void Awake()
+    {
+        _resetPos = this.transform.position;
+    }
 
     public void SetInfo(string text, float lifeTime)
     {
+        this.transform.position = _resetPos;
+        _text.color = new Color(1, 1, 1, 1);
         _text.text = text;
         _lifeTime = lifeTime;
 
