71bea5e372b51e306ca15883f6332510f84cb487
mjun1208 <mjun1208@gmail.com>
2021-03-30 00:42:47 +0900

    UIPopupManager 추가

---
+++ Assets/Scripts/Manager/UIPopupManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PopupID
+{
+    UIWakWindow,
+    UIInventory,
+    UIUnitWindow,
+}
+
+public class UIPopupManager : MonoBehaviour
+{
+
+    public void Push(PopupID id)
+    {
+
+    }
+}
