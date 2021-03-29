3d0a55b37158c18143dbfb1a2bf6a2d85a95729e
mjun1208 <mjun1208@gmail.com>
2021-03-30 01:32:46 +0900

    UIPopup 추가 (팝업 전용 스크립트)

---
+++ Assets/Scripts/UI/UIPopup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class UIPopup : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public abstract PopupID GetPopupID();
+
+    protected RectTransform _rectTransform;
+
+    private Vector3 _dragPivot = Vector3.zero;
+
+    private void Start()
+    {
+        _rectTransform = this.GetComponent<RectTransform>();
+    }
+
+    public void Pop()
+    {
+        Global.ObjectPoolManager.CanvasObjectPooling(this.GetPopupID().ToString(), this.gameObject);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = Input.mousePosition + _dragPivot;
+        this.transform.localPosition = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y, 0);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _dragPivot = this.transform.position - Input.mousePosition;
+        IngameManager.UnitManager.ControlOn = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        IngameManager.UnitManager.ControlOn = true;
+    }
+}
