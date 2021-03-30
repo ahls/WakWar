d367e65fe6e0d951625396dfabda8a77afb5b3ba
mjun1208 <mjun1208@gmail.com>
2021-03-31 02:20:42 +0900

    메뉴 바 스크립트 추가

---
+++ Assets/Scripts/UI/UIMenuBar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMenuBar : MonoBehaviour
+{
+    public void PushUIWakWindow()
+    {
+        if (Global.UIPopupManager.FindPopup(PopupID.UIWakWindow) == null)
+        {
+            Global.UIPopupManager.Push(PopupID.UIWakWindow);
+        }
+    }
+}
