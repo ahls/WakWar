d7d7faff98e42a469ef30f68cb2ed3f7ce42640a
mjun1208 <mjun1208@gmail.com>
2021-04-01 02:10:14 +0900

    인벤토리 UIPopup화

---
+++ Assets/Scripts/UI/Popup/UIInventory.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInventory : UIPopup
+{
+    public override PopupID GetPopupID() { return PopupID.UIInventory; }
+}
