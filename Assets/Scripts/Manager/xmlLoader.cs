30a7203be307e53e3f3d940cbf3ee8bf1f70b5b6
minjun <mjun1208@naver.com>
2021-04-06 02:43:08 +0900

    스테이지 로딩!

--- Assets/Scripts/Manager/xmlLoader.cs
+++ Assets/Scripts/Manager/xmlLoader.cs
@@ -6,12 +6,14 @@ public class xmlLoader : MonoBehaviour
 {
     public const string weaponPath = "weapons";
     public const string itemPath = "items";
+    public const string StagePath = "stage_xml";
 
     // Start is called before the first frame update
     void Awake()
     {
         WeaponContainer.Load(weaponPath);
         ItemContainer.Load(itemPath);
+        StageXML.Load(StagePath);
     }
 
 }
