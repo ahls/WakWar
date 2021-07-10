9fc9be74b528ee8216a2686f3217944ee8c54cb0
minjun <mjun1208@naver.com>
2021-07-11 04:04:57 +0900

    범용성 이펙트 스크립트 추가

---
+++ Assets/Scripts/Effect.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    private string _prefabName = "";
+    private ParticleSystem _particle = null;
+    private Animator _anime = null; 
+    private float _lifeTime = 0f;
+
+    private void OnEnable()
+    {
+        _particle = GetComponent<ParticleSystem>();
+        _anime = GetComponent<Animator>();
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+        _prefabName = "";
+        _particle = null;
+        _anime = null;
+        _lifeTime = 0f;
+    }
+
+    public void SetEffectInfo(string name, float lifeTime = 0f)
+    {
+        _lifeTime = lifeTime;
+
+        if (_lifeTime != 0f)
+        {
+            StartCoroutine(ActiveLifeTime());
+        }
+    }
+
+    private IEnumerator ActiveLifeTime()
+    {
+        yield return new WaitForSeconds(_lifeTime);
+
+        DestroyEffect();
+
+        yield return null;
+    }
+
+    private void Update()
+    {
+        if (_particle != null && !_particle.isPlaying)
+        {
+            DestroyEffect();
+        }
+
+        if (_anime != null && _anime.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.95f)
+        {
+            DestroyEffect();
+        }
+    }
+
+    private void DestroyEffect()
+    {
+        StopAllCoroutines();
+
+        Global.ObjectPoolManager.ObjectPooling(_prefabName, gameObject);
+    }
+}
